48e5bf76b745434fa5caf5f7fe6ce5de9c8003d5
Ryan Lin <RyanL68@nycstudents.net>
2022-10-04 16:56:49 -0400

    added a print statement

---
+++ src/RectangleRunner.java
@@ -0,0 +1,9 @@
+public class RectangleRunner {
+    public static void main(String[] args){
+     Rectangle rect1 = new Rectangle(10,20);
+
+
+        System.out.print("This rectangle has an area of "+rect1.printArea()+" and the volume is " +rect1.printBoxVolume(25));
+    }
+
+}
